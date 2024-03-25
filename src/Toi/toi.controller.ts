eff1074e680be590db238243db80109f2e62f94d
Elisei Razgyulaev <elisei99@list.ru>
2024-03-25 12:22:16 +0300

    Добавил env и в него кта

--- src/Toi/toi.controller.ts
+++ src/Toi/toi.controller.ts
@@ -4,6 +4,7 @@ import { Logger } from '@nestjs/common';
 import { Toi } from 'src/db/models/toi.model';
 import { flatOffsetMeterToLongitudeLatitude } from 'src/Utils/XYtoLanLon';
 import { Formular } from 'src/db/models/Formular.model';
+require('dotenv').config();
 
 @Controller('toi')
 export class ToiController {
@@ -21,10 +22,12 @@ export class ToiController {
     try {
       const toi = await this.toiModel.findAll();
       const toiFiltered = toi.filter((item) => item.Number !== 0);
+      const ctalat = process.env.ctalat;
+      const ctalon = process.env.ctalon;
       const formattedToi = await Promise.all(toiFiltered.map(async (item) => {
         const formular = await this.formularModel.findAll({where: {id: item.id_Sintez}});
         const [lat, lon] = flatOffsetMeterToLongitudeLatitude(
-          [30.266975,59.800364], 
+          [Number(ctalon), Number(ctalat)], 
           item.Y, 
           item.X
         );
