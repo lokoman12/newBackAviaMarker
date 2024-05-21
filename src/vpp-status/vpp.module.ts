7b8b5d703b2899ec5994122a5c8b9a62565ce50b
Elisei Razgyulaev <elisei99@list.ru>
2024-05-21 09:57:19 +0300

    Обновил впп статусы сделал публичными и создал контроллер чтобы менять статус

--- src/vpp-status/vpp.module.ts
+++ src/vpp-status/vpp.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import VppStatus from 'src/db/models/vppStatus.model';
 import { VppStatusController } from './vpp.status.controller';
+import { VppChangeStatusController } from './vpp.status.change.controller';
 
 
 @Module({
   imports: [SequelizeModule.forFeature([VppStatus])],
-  controllers: [VppStatusController],
+  controllers: [VppStatusController, VppChangeStatusController],
   exports: [SequelizeModule],
 })
 export class VppStatusModule {}
---
+++ src/vpp-status/vpp.status.change.controller.ts
@@ -0,0 +1,58 @@
+import { Controller, Post, Query, Body } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Logger } from '@nestjs/common';
+import VppStatus from 'src/db/models/vppStatus.model';
+import { AccessTokenGuard } from '../auth/guards/access.token.guard';
+import { UseGuards } from '@nestjs/common';
+import { Public } from 'src/auth/decorators/public.decorator';
+
+@Controller('vppStatusChange')
+export class VppChangeStatusController {
+  private readonly log = new Logger(VppChangeStatusController.name);
+
+  constructor(
+    @InjectModel(VppStatus) private readonly vppStatusModel: typeof VppStatus,
+  ) {
+    this.log.log('Init controller');
+  }
+
+  @Public()
+  // @UseGuards(AccessTokenGuard)
+  @Post()
+  async updateVppStatus(
+    @Query('name') name: string,
+    @Query('status') status: string,
+  ): Promise<any> {
+    try {
+      if (status === 'close') {
+        const vppStatusUpdate = await this.vppStatusModel.update(
+          {
+            close_vpp: 1,
+            // RegimL: 5,
+            // RegimR: 5,
+          },
+          {
+            where: { vpp_name: name },
+          },
+        );
+        return vppStatusUpdate;
+      }
+      if (status === 'open') {
+        const vppStatusUpdate = await this.vppStatusModel.update(
+          {
+            close_vpp: 0,
+            // RegimL: 10,
+            // RegimR: 10,
+          },
+          {
+            where: { vpp_name: name },
+          },
+        );
+        return vppStatusUpdate;
+      }
+    } catch (error) {
+      this.log.error('Error updating VppStatus:', error);
+      throw error;
+    }
+  }
+}
